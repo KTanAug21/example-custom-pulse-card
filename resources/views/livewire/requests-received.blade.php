04fba82511de36723b671f1f7856bdbd19af5af4
Kathryn Anne S Tan <kathryn@fly.io>
2024-02-08 16:17:55 +1300

    add style to custom card

--- resources/views/livewire/requests-received.blade.php
+++ resources/views/livewire/requests-received.blade.php
@@ -1,10 +1,41 @@
-<x-pulse::card :cols="$cols" :rows="$rows" :class="$class" >
+<x-pulse::card :cols="$cols" :rows="$rows" :class="$class" wire:poll.5s="">
     <x-pulse::card-header name="Request Distribution">
+        <x-slot:icon>
+            <x-pulse::icons.rocket-launch />
+        </x-slot:icon>
     </x-pulse::card-header>
-
+ 
     <x-pulse::scroll :expand="$expand">
-        @foreach( $machines as $machine )
-            {{ $machine->key }} = {{ $machine->count }}
-        @endforeach
+        <div>
+            <x-pulse::table>
+                <colgroup>
+                    <col width="100%" />
+                    <col width="0%" />
+                </colgroup>
+                <x-pulse::thead>
+                    <tr>
+                        <x-pulse::th>Machine ID</x-pulse::th>
+                        <x-pulse::th class="text-right">Requests</x-pulse::th>
+                    </tr>
+                </x-pulse::thead>
+                <tbody>
+                    @foreach( $machines as $machine )
+                        <tr>
+                            <x-pulse::td class="max-w-[1px]">
+                                <code class="block text-xs text-gray-900 dark:text-gray-100 truncate">
+                                    {{ $machine->key }}
+                                </code>
+                            </x-pulse>
+                            <x-pulse::td class="max-w-[1px]">
+                                <code class="block text-xs text-gray-900 dark:text-gray-100 truncate">
+                                    {{ $machine->count }}
+                                </code>
+                            </x-pulse>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </x-pulse::table>
+        </div>
+        
     </x-pulse::scroll>
 </x-pulse::card>
